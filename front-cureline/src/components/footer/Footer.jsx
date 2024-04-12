b8c1a00bd637c0fdb24298292d8054ecf0a93128
tilekl01 <zylkycievtilek@gmail.com>
2024-04-12 13:04:07 +0600

    ewr

--- front-cureline/src/components/footer/Footer.jsx
+++ front-cureline/src/components/footer/Footer.jsx
@@ -98,7 +98,6 @@ const Footer = () => {
 					<div className="row">
 						<div className="col-lg-12 col-md-12 col-12">
 							<div className="copyright-content">
-								<p>© Copyright 2024  |  All Rights Reserved by  </p>
 							</div>
 						</div>
 					</div>
